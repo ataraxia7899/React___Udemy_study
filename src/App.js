2a6b8855d71848ca8914269fa1ef029cc6a9ca69
ataraxia7899 <ataraxia7899@gmail.com>
2024-12-18 02:05:57 +0900

    기초프로젝트 진행한 내용 저장

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from "react";
-import Card from "./components/UI/Card.js";
-import ExpenseItem from "./components/Expenses/ExpenseItem.js"
-import "./index.css"
-import expenses from "./expenses.js"
+import Header from "./components/Header/Header.js";
+import Concept from "./components/Concept/Concept.js";
+import Concepts from "./Concepts.js"
 
-const App = () => {
-    return (
-        <Card className="expense_components">
-            <h2 className="app_title">지출 목록</h2>
-            {expenses.map((expense) => (
-                /* JS의 map을 활용해 배열의 
-                각 원소를 컴포넌트로 대체해 반환 */
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))}
-        </Card>
-    );
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <div id="concepts">
+        {Concepts.map((Con) => (
+          <Concept 
+            title = {Con.title}
+            image = {Con.image}
+            description = {Con.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-
-export default App;
