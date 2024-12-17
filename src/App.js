f9c3773487267665f65195fa43e74ba3120e0e19
ataraxia7899 <ataraxia7899@gmail.com>
2024-12-18 01:22:20 +0900

    basic__다른 프로젝트에 저장되있던 연습내용을 브랜치로 저장장

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./components/UI/Card.js";
+import ExpenseItem from "./components/Expenses/ExpenseItem.js"
+import "./index.css"
+import expenses from "./expenses.js"
+
+const App = () => {
+    return (
+        <Card className="expense_components">
+            <h2 className="app_title">지출 목록</h2>
+            {expenses.map((expense) => (
+                /* JS의 map을 활용해 배열의 
+                각 원소를 컴포넌트로 대체해 반환 */
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))}
+        </Card>
+    );
+}
+
+export default App;
---
+++ src/components/UI/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Card.css"
+
+// 모든 컴포넌트를 감싸는 껍질형태의 컴포넌트
+const Card = (props) => {
+    const classes = "card " + props.className;
+
+    return (
+        <div className={classes}>{props.children}</div>
+    );
+}
+
+export default Card;
