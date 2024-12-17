a0a45481865f7ed7e368eb0276c12aa4f1ba729d
ataraxia7899 <ataraxia7899@gmail.com>
2024-12-17 18:46:46 +0900

    필터링 값 없을때의 if조건식 추가가

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -37,14 +37,19 @@ const Expenses = (props) => {
 
             {/* title값과 amount값같은 값들을 동적으로 자동 배정해줌 */}
             {/* filteredExpenses를 통해 년도값이 같은 소비목록만 조회됨 */}
-            {filteredExpenses.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))}
+            {/* 조건 검색이 안되면 if값의 <p>태그 출력 */}
+            {filteredExpenses.length === 0 ? (
+                <p> 조건에 맞는 값이 조회되지 않습니다. </p>
+            ) : (
+                filteredExpenses.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))
+            )}
         </Card>
     );
 };
